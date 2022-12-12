5347e927c7ea324ea940d5240699769860df3fb3
kaykyveiga <kaykyveiga46@gmail.com>
2022-12-12 08:22:49 -0300

    Condições

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidadee do carro é de ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre com responsabilidade!')
